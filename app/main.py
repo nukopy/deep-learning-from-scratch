a477d0999c8bff9cf3504717f03601e8e4a7947b
nukopy <nukopy@gmail.com>
2023-06-10 02:30:09 +0900

    chore: format

--- app/main.py
+++ app/main.py
@@ -1,12 +1,15 @@
 def add(x: int, y: int) -> int:
     return x + y
 
+
 def sub(x: int, y: int) -> int:
     return x - y
+
 
 def main():
     print(add(2, 3))
     print(sub(2, 3))
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
--- tests/test_main.py
+++ tests/test_main.py
@@ -4,5 +4,6 @@ from app.main import add, sub
 def test_add():
     assert add(2, 3) == 5
 
+
 def test_sub():
     assert sub(2, 3) == -1
